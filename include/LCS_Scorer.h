e56443806227d36da6a1f15b907ba6d0385f9633
HN <henry2833@gmail.com>
2019-05-30 22:30:22 -0700

    rem leftovers

--- include/LCS_Scorer.h
+++ include/LCS_Scorer.h
@@ -1,8 +1,10 @@
 #ifndef LCS_Scorer
 #define LCS_Scorer
 #include <string>
+
 namespace LCS{
 double maxf(double a, double b);
 double lcs_compute(std::string X, std::string Y);
+std::vector<std::vector<double>> init2 (std::string pref, std::string suff);
 }
 #endif
