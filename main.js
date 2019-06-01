c5a81fae3ea3abd74b4f6de79cd615fd3e92eaa0
HN <henry2833@gmail.com>
2019-05-31 22:02:55 -0700

    Set up server. Upload + path renaming possible.

--- main.js
+++ main.js
@@ -5,28 +5,23 @@ $(document).ready(function(){
     var text = $("#name").val();
     var url;
     $( '#load-file' ).click( function () {
-        if ( ! window.FileReader ) {
-            return alert( 'FileReader API is not supported by your browser.' );
-        }
-        var $i = $( '#name' ), // Put file input ID here
-            input = $i[0]; // Getting the element from jQuery
-        var file = input.files[0]; // The file
-        var fr = new FileReader(); // FileReader instance
-        if(!input.files || !input.files[0]) {
-            console.log ("Bad\n");
-        }
-        var text="first";
-        fr.onload= function () {
-            console.log("called: " + fr.result);
-            text = fr.result;
-            console.log ( "result: " + text );
-            url = "http://localhost:3000/" + text;
-            $.get(url, function(data, status){
-                alert("Data: " + data + "\nStatus: " + status);
-            });
-        }
-        fr.readAsText( file );
+        var formD = new FormData();
+        formD.append('file', $("#name").get(0).files[0])
+        $.ajax({
+            url:"http://localhost:3000",
+            data: formD,
+            processData: false,
+            contentType: false,
+            type: 'POST',
+            success: function(data) {
+                $("#ls").append(data);
+            }
+        })
+        // $.post("http://localhost:3000", formD, function(data) {
+        //     $("#ls").append(data);
+        //     alert("alert: " + data);
+        // })
     } );
-    $("button").click(function(){
-    });
+$("button").click(function(){
+});
 });
--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 const http = require('http');
+var formidable = require ('formidable'), util = require('util');
+var fs = require('fs');
+var jsonString ='';
+var _PATH = "/Users/user/documents/github/SCS/";
+console.log("path exists: " + fs.existsSync("./a.txt"));
+console.log("2path exists: " + fs.existsSync("/Users/user/data"));
 const server = http.createServer((request, response) => {
     console.log("Connection: " + request.url);
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    response.write(request.url);
-    response.end();
+    if(request.method == 'POST') {
+        fs.rename("~/test","~/test1", function (err) {
+            if(err) console.log("error: "+ err);});
+
+
+        var form = new formidable.IncomingForm();
+        form.keepFilenames = true;
+        form.on('file', function(name, file) {
+            console.log("file received= " + name + "/ path is : "+ file.path);
+            console.log("exists: "+ fs.existsSync(file.path));
+            fs.rename(file.path, _PATH + "data/input.txt", function (err) {
+                if(err) console.log("error: "+ err);
+            });
+        });
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        form.parse(request, function(err, fields, files) {
+            console.log ("PARSING\n");
+            response.writeHead(200, {'content-type': 'text/plain'});
+            response.write("received\n\n\n");
+            response.end();
+        });
+        // response.write(request.url);
+        // response.write("connection achieved\n");
+    }
 });
 server.listen(3000);
