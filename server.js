9e3ca88e3efbccd4d99c9b87c23e5e358f98b8bf
HN <henry2833@gmail.com>
2019-06-01 20:06:52 -0700

    good

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
 const http = require('http');
 var formidable = require ('formidable'), util = require('util');
-var fs = require('fs');
+var fs = require('fs'), {execFile} = require ('child_process');
 var jsonString ='';
-var _PATH = "/Users/user/documents/github/SCS/";
-console.log("path exists: " + fs.existsSync("./a.txt"));
-console.log("2path exists: " + fs.existsSync("/Users/user/data"));
 const server = http.createServer((request, response) => {
+    var _PATH = "/Users/user/documents/github/SCS/", output;
     console.log("Connection: " + request.url);
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.writeHead(200, {'content-type': 'text/plain'});
     if(request.method == 'POST') {
-        fs.rename("~/test","~/test1", function (err) {
-            if(err) console.log("error: "+ err);});
-
-
         var form = new formidable.IncomingForm();
-        form.keepFilenames = true;
+        form.parse(request, function(err, fields, files) {});
+
+
+
         form.on('file', function(name, file) {
-            console.log("file received= " + name + "/ path is : "+ file.path);
-            console.log("exists: "+ fs.existsSync(file.path));
-            fs.rename(file.path, _PATH + "data/input.txt", function (err) {
-                if(err) console.log("error: "+ err);
+            fs.rename(file.path, _PATH + "data/" + file.name, function (err) { if(err) console.log("error: "+ err); });
+            _PATH +="data/" + file.name;
+            console.log(_PATH);
+            child = execFile('/Users/user/documents/github/SCS/bin/SCS', [_PATH], (error, stdout, stderr)=> {
+                if(stderr) {
+                    response.write("-1\n", function (err) {response.end()});
+                    console.log ("error: " + error);
+                }
+                response.write("0\n" + stdout , function (err) {
+                    response.end();
+                });
             });
-        });
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        form.parse(request, function(err, fields, files) {
-            console.log ("PARSING\n");
-            response.writeHead(200, {'content-type': 'text/plain'});
-            response.write("received\n\n\n");
-            response.end();
         });
         // response.write(request.url);
         // response.write("connection achieved\n");
